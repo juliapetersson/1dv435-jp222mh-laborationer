344aecbe38c214e78b7ae452f090483b3e802f1c
Julia Petersson <juliapetersson@hotmail.se>
2013-12-18 02:39:12 -0500

    lite mer javakod

--- js.js
+++ js.js
@@ -1,5 +1,30 @@
+//.labbtable {
+//	display: none;
+//}
 
+var div = document.querySelector("#resultat");
+div.style.color= "#000000";
+var a = div.querySelector("a");
+//var table = document.getElementById("hidden");
+var table = document.getElementById("hidden"); 
+table.className = "labbtable";
 
+//for(var i = 0; i != table.length; i++){
+
+    if(table.className = "labbtable"){
+         a.onclick = function () {
+        //table.style.display ="";
+        table.setAttribute("class", "jschanged"); 
+         };
+    }
+    else{
+         a.onclick = function () {
+         //table.style.display ="none";
+        table.setAttribute("class", "labbtable");
+         };
+    
+    }
+//}
 
 // http://juliapetersson.github.io/1dv435-jp222mh-laborationer/pics/bobbysailor.jpg#bobby
 // http://juliapetersson.github.io/1dv435-jp222mh-laborationer/pics/cupcakegirl.png#cupcake
@@ -9,5 +34,5 @@
 //console.log(myImages[0]); <-- Fungerar!
 
 
-//Bilden uppdaterar du genom att via JavaScript ändra bild-elementets src-attribut: document.getElementById
+//Bilden uppdaterar du genom att via JavaScript ï¿½ndra bild-elementets src-attribut: document.getElementById
 //("idOnImageElement").src = img_arr[counter];
--- pages/valjs.js
+++ pages/valjs.js
@@ -4,17 +4,29 @@ var a = form.querySelector("a");
 var p = form.querySelector("p");
 var input = form.querySelector("input");
 
-a.onclick = function () {
-	p.innerHTML = input.value;
-
-};
+//a.onclick = function () {
+//p.innerHTML = input.value * 9.04;
+    //p.innerHTML = input.value * 6.57;
+//};
+//arr.push(element1, ..., elementN)
+//arr.reverse()
 var valuelength = input.value.length;
-console.log(valuelength);
 
-if(valuelength<1){
+var svenska = (" svenska kronor blir");
+var euro = (" euro och");
+var dollar = (" dollar.");
+//var allt = input.value +svenska+" "+(input.value * 9.04) +euro+" "+(input.value * 6.57) +dollar;
 
-	alert("Du har glömt skriva in några siffror");
+if(isNaN(input.value)){
+    a.onclick = function () {
+        p.innerHTML = input.value +svenska+" "+(input.value * 9.04) +euro+" "+(input.value * 6.57) +dollar;
+	//p.innerHTML = allt;
+	//p.innerHTML = ("Du har glömt att skriva in några siffror");
+	console.log(input.value);
+    };
 }
 else{
-	alert("Rätt");
+    a.onclick = function () {
+	p.innerHTML = ("Du har skrivit in något");
+    };
 }
