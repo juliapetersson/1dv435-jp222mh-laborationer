2704a795ae4e72fce84ea22842463a1db832fd6f
juliapetersson <jp222mh@student.lnu.se>
2014-01-09 12:26:31 +0100

    ett nytt försök

--- bildspel.js
+++ bildspel.js
@@ -1,9 +1,15 @@
 
-var bildspel = document.getElementById("bildspel");
-
-var myPics = [http://farm8.staticflickr.com/7051/6932077670_34884676d5_o.jpg, https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTGgIt4wLiuOzHo7hfhta8Sm28q1JT_RxTx5HHdnk9J3N-LvVwexw, http://www.google.se/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&docid=vWz1OqxS0B01GM&tbnid=tIlfV7PaLDt1PM:&ved=0CAUQjRw&url=http%3A%2F%2Finchmark.squarespace.com%2Finchmark%2F2009%2F2%2F5%2Fart-for-the-people.html&ei=sEvNUt6kGem64ASWooC4Aw&bvm=bv.58187178,d.bGE&psig=AFQjCNFNRa0qxW2s0GrqQeFQiPSbD0BZTw&ust=1389272282750480]
+var fart = 1000;
+var bilder = new Array();
+//var myPics = [http://farm8.staticflickr.com/7051/6932077670_34884676d5_o.jpg, https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTGgIt4wLiuOzHo7hfhta8Sm28q1JT_RxTx5HHdnk9J3N-LvVwexw, http://www.google.se/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&docid=vWz1OqxS0B01GM&tbnid=tIlfV7PaLDt1PM:&ved=0CAUQjRw&url=http%3A%2F%2Finchmark.squarespace.com%2Finchmark%2F2009%2F2%2F5%2Fart-for-the-people.html&ei=sEvNUt6kGem64ASWooC4Aw&bvm=bv.58187178,d.bGE&psig=AFQjCNFNRa0qxW2s0GrqQeFQiPSbD0BZTw&ust=1389272282750480]
+bilder[0] = "http://farm8.staticflickr.com/7051/6932077670_34884676d5_o.jpg";
+bilder[0] = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTGgIt4wLiuOzHo7hfhta8Sm28q1JT_RxTx5HHdnk9J3N-LvVwexw";
+bilder[0] = "http://www.google.se/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&docid=vWz1OqxS0B01GM&tbnid=tIlfV7PaLDt1PM:&ved=0CAUQjRw&url=http%3A%2F%2Finchmark.squarespace.com%2Finchmark%2F2009%2F2%2F5%2Fart-for-the-people.html&ei=sEvNUt6kGem64ASWooC4Aw&bvm=bv.58187178,d.bGE&psig=AFQjCNFNRa0qxW2s0GrqQeFQiPSbD0BZTw&ust=1389272282750480";
 
 var count = 0;
+
+
+
 
 function (){
 
